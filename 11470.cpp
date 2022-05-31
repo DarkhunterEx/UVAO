24bd1b5dcad43763224d27c9a506668988f2b5b1
DarkhunterEx <106594935+DarkhunterEx@users.noreply.github.com>
2022-05-31 09:44:14 -0300

    Update 11470.cpp

--- 11470.cpp
+++ 11470.cpp
@@ -5,29 +5,33 @@ int z;
 bool b[10][10];
 int a[10][10];
 int ff(int i,int j,int l,int m){
-	cout<<i<<" "<<j<<" "<<l<<" "<<m<<endl;
 	if(i+1<z-l and b[i+1][j]!=true){
+		if(i+1==l or j==z-l-1 or j==l or i+1==z-l-1){
 		b[i+1][j]=true;
-		return ff(i+1,j,l,m+a[i+1][j]);
+		return ff(i+1,j,l,m+a[i+1][j]);}
 	}
 	if(j+1<z-l and b[i][j+1]!=true){
+		if(i==l or j+1==z-l-1 or j+1==l or i==z-l-1){
 		b[i][j+1]=true;
-		return ff(i,j+1,l,m+a[i][j+1]);
+		return ff(i,j+1,l,m+a[i][j+1]);}
 	}
 	if(i-1>=0+l and b[i-1][j]!=true){
+		if(i-1==l or j==z-l-1 or j==l or i-1==z-l-1){
 		b[i-1][j]=true;
-		return ff(i-1,j,l,m+a[i-1][j]);
+		return ff(i-1,j,l,m+a[i-1][j]);}
 	}
-	if(j-1>=0+l and a[i][j-1]!=true){
-		a[i][j-1]=true;
-		return ff(i,j-1,l,m+a[i][j-1]);
+	if(j-1>=0+l and b[i][j-1]!=true){
+		if(i==l or j-1==z-l-1 or j-1==l or i==z-l-1){
+		b[i][j-1]=true;
+		return ff(i,j-1,l,m+a[i][j-1]);}
 	}
-	return l;
+	return m;
 }
 
 int main(){
-	//freopen("12.txt","w",stdout);
+	freopen("12.txt","w",stdout);
 	cin>>z;
+	int i=1;
 	while(z!=0){
 		for(int i=0;i<10;i++){
 			for(int j=0;j<10;j++){
@@ -40,10 +44,23 @@ int main(){
 				b[i][j]=false;
 			}
 		}
-		for(int i=0;i<z/2;i++){
+		cout<<"Case "<<i<<": ";
+		if(z%2!=0){
+			b[0][0]=true;
+			cout<<ff(0,0,0,a[0][0]);
+			for(int i=1;i<=z/2;i++){
 			b[i][i]=true;
-			cout<<ff(i,i,i,a[i][i])<<endl;
+			cout<<" "<<ff(i,i,i,a[i][i]);}
 		}
+		else{
+			b[0][0]=true;
+			cout<<ff(0,0,0,a[0][0]);
+			for(int i=1;i<z/2;i++){
+			b[i][i]=true;
+			cout<<" "<<ff(i,i,i,a[i][i]);}
+		}
+		cout<<endl;
+		i++;
 		cin>>z;
 	}
 }
